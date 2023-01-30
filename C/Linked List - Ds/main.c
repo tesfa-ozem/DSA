d775653c4da6df5a8f419f476c15f07c57be91fe
tesfa-ozem <alphatesfa789@gmail.com>
2023-01-30 23:30:28 +0300

    Linked List basic operations: - size() - returns number of data elements in list - empty() - bool returns true if empty - value_at(index) - returns the value of the nth item (starting at 0 for first) - push_front(value) - adds an item to the front of the list - pop_front() - remove front item and return its value - push_back(value) - adds an item at the end - pop_back() - removes end item and returns its value - front() - get value of front item - back() - get value of end item - insert(index, value) - insert value at index, so current item at that index is pointed to by new item at index - erase(index) - removes node at given index - value_n_from_end(n) - returns the value of the node at nth position from the end of the list

--- C/Linked List - Ds/main.c
+++ C/Linked List - Ds/main.c
@@ -19,37 +19,33 @@ void printList(Node *n)
     }
     printf("\n");
 }
-// Insert operation at nth index
 
-void insertNodeAtIndex(int data, Node *n,int index)
+// Insert operation at nth index
+void insertNodeAtIndex(int data, Node *n, int index)
 {
     int count = 1;
-    Node* prev;
-    Node* next;
-    if(index==0){
-        Node *newNode = (Node *)malloc(sizeof(Node));
-        newNode->data = data;
-        newNode->next = n;
-        head = newNode;
-    }else{
-        while (next!=NULL)
+    Node *prev = NULL;
+    Node *temp = n;
+    while (temp != NULL)
+    {
+        if (count == index)
         {
-            
-            prev = n;
-            next = prev->next;
-            if(count!=index)
+            Node *newNode = (Node *)malloc(sizeof(Node));
+            newNode->data = data;
+            newNode->next = temp;
+            if (prev == NULL)
             {
-               n = n->next;
-            }else
-            {
-                Node *newNode = (Node *)malloc(sizeof(Node));
-                prev->next = newNode;
-                newNode->next = next;
-                newNode->data = data;
+                head = newNode;
             }
-            count++;
+            else
+            {
+                prev->next = newNode;
+            }
+            break;
         }
-        
+        prev = temp;
+        temp = temp->next;
+        count++;
     }
 }
 
@@ -68,15 +64,14 @@ void insertNode(int data, Node *n)
         Node *newNode = (Node *)malloc(sizeof(Node));
         while (n->next != NULL)
         {
-
             n = n->next;
         }
         n->next = newNode;
         newNode->data = data;
     }
 }
-// Pop item from front
 
+// Pop item from front
 int popFront(Node *h)
 {
     int value = h->data;
@@ -87,37 +82,33 @@ int popFront(Node *h)
 }
 
 // Pop item from back
-
 int popBack(Node *h)
 {
-    Node* temp;
-    temp = h;
-    Node * prev;
+    Node *temp = h;
+    Node *prev = NULL;
     int value;
-    while (temp!=NULL)
+    while (temp != NULL)
     {
-        if(temp->next!=NULL)
+        if (temp->next == NULL)
         {
-            prev = temp;
-            temp = temp->next;
-        }
-        else
-        {
-            prev->next = NULL;
+            if (prev == NULL)
+            {
+                head = NULL;
+            }
+            else
+            {
+                prev->next = NULL;
+            }
             value = temp->data;
             free(temp);
             return value;
         }
-        
-
+        prev = temp;
+        temp = temp->next;
     }
-    // 
-    
-    
 }
 
 // Get the size of the linkedlist
-
 int getSize(Node *h)
 {
     if (h == NULL)
@@ -130,27 +121,88 @@ int getSize(Node *h)
     }
 }
 
-// return the value at nth index
-
-Node* value_at(int index, Node *h)
+// Return the value at nth index
+Node *value_at(int index, Node *h)
 {
-    int postion = 0;
+    int position = 0;
     Node *temp = h;
-    while (h != NULL)
+    while (temp != NULL)
     {
-        if (postion != index)
-        {
-            temp = temp->next;
-        }else
+        if (position == index)
         {
             return temp;
         }
-        postion++;
-        
+        position++;
     }
-
     
 }
+
+// Get value at nth index
+Node* back(Node *h)
+{
+    Node *temp = h;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    printf("%d \n",temp->data);
+    return temp;
+}
+
+// erase(index) - removes node at given index
+
+void erase(int index, Node *h)
+{
+    int count = 1;
+    Node* temp = h->next;
+    Node* previous = h;
+    
+    while (temp->next!= NULL)
+    {
+        if(count==index)
+        {
+            
+            previous->next = temp->next;
+            free(temp);
+        }else
+        {
+            
+            temp = temp->next;
+            previous = previous->next;
+            
+        }
+        count++;
+    }
+    
+}
+
+// value_n_from_end(n) - returns the value of the node at nth position from the end of the list
+
+Node* value_n_from_end(int N, Node *h){
+    int len = 0, i;
+    Node* temp = h;
+  
+    // Count the number of nodes in Linked List
+    while (temp != NULL) {
+        temp = temp->next;
+        len++;
+    }
+  
+    // Check if value of N is not
+    // more than length of the linked list
+    if (len < N)
+        return NULL;
+    temp = h;
+  
+    // Get the (len-N+1)th node from the beginning
+    for (i = 1; i < len - N + 1; i++)
+        temp = temp->next;
+    printf("%d \n\n", temp->data);
+    return temp;
+    
+
+}
+
 int main(int argc, char const *argv[])
 {
     // int val = 2;
@@ -162,10 +214,12 @@ int main(int argc, char const *argv[])
     insertNode(6, head);
     // Node * val = value_at(1,head);
     // popFront(head);
-    popBack(head);
+    // popBack(head);
+    // erase(3,head);
+    // back(head);
     // insertNodeAtIndex(7,head, 10);
     printList(head);
-    // printf("List length = %d \n\n", getSize(head));
-    // printf("Index of 2 is = %d", value_at(2,head)->data);
+    printf("List length = %d \n\n", getSize(head));
+    printf("Index of 1 is = %d", value_n_from_end(1,head)->data);
     return 0;
 }
